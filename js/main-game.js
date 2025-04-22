efd1fc5c096d3a2b7a3170080fbef2e42f9de7f4
VithusanU <130177580+VithusanU@users.noreply.github.com>
2025-04-21 20:57:25 -0400

    using api to pull word

--- js/main-game.js
+++ js/main-game.js
@@ -7,6 +7,8 @@ const wordWindow = document.getElementById('word-selection-window');
 const sloganMode = document.getElementById('slogan-mode');
 const sloganWindow = document.getElementById('slogan-selection-window');
 const gameArea = document.getElementById('game');
+const wordBank = [];
+
 
 //This works with the start button/mode buttons
 startGameButton.addEventListener("click", () => {
@@ -41,8 +43,18 @@ sloganMode.addEventListener('click', () => {
 //This will be the game logic
 
 
-
-
+async function getRandomWord() {
+    try {
+        const response = await fetch ('https://random-word-api.herokuapp.com/word?number=1') // fetches response
+        const data = await response.json(); //This converts the raw HTTP response into a JavaScript object/array.
+        const word = data[0]; // Extracts the first word
+        console.log('Random Word:', word);
+        return word.toLowerCase();
+    } catch (error) {
+        console.error('Failed to get word:', error);
+        return 'fallback';
+    }
+}
 
 
 
